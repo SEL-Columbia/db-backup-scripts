e95ac2790353544341a2a160581328ba6f267cc0
sushmitharaos <sushmitharaos@gmail.com>
2014-12-23 13:30:08 +0530

    Thiru, Sush | updatind the backup scripts to not copy the folders for couchdb

--- couchdb-scripts/couch-backup.sh
+++ couchdb-scripts/couch-backup.sh
@@ -8,33 +8,35 @@ backup_dest="$parent_dir/couchdb-$today"
 rm -rf $parent_dir/couchdb*
 
 echo "Created a directory $backup_dest"
-mkdir -p $backup_dest/conf $backup_dest/logs $backup_dest/data
-
-echo "Copying CouchDB configuration files"
-
-cp -rp /etc/couchdb $backup_dest/conf
-
-echo "Copying CouchDB Log files"
-
-cp -rp /var/log/couchdb $backup_dest/logs
-
-echo "Copying CouchDB Database files"
-
-cp -rp /var/lib/couchdb $backup_dest/data
+mkdir -p $backup_dest
 
 echo "Archiving the backup files"
 
+cd $backup_dest
+
+echo "Archiving CouchDB configuration files"
+
+tar -Pczf "couchdb-config.$today.tar" "/etc/couchdb"
+
+echo "Archiving CouchDB Log files"
+
+tar -Pczf "couchdb-log.$today.tar" "/var/log/couchdb"
+
+echo "Archiving CouchDB Database files"
+
+tar -Pczf "couchdb-data.$today.tar" "/var/lib/couchdb"
+
 cd $backup_dest/..
 
-tar -cf "couchdb-$today.tar" "couchdb-$today"
+echo "Archiving Everything"
+
+tar -czf "couchdb-$today.tar" "$backup_dest"
 
 echo "Zipping and Encrypting the backup files"
 
 echo $1 | gpg  --batch --no-tty --yes --passphrase-fd 0 -o "couchdb-$today.tar.zip" --cipher-algo aes256 --compress-algo zip --symmetric "couchdb-$today.tar"
 
-rm -rf $backup_dest
-
-rm -rf "couchdb-$today.tar"
+rm -rf "couchdb-$today.tar" 
+rm -rf "$backup_dest"
 
 echo "Done archiving $backup_dest directory"
-
